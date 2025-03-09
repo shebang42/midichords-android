34c99400bf8a4b72ff694f755261a9f8b0b19fb4
Paul <git@postada.nl>
2025-03-10 00:16:45 +0100

    Implement alternative chord names, slash chord and jazz chord notation with comprehensive test suite

--- app/src/main/java/com/midichords/model/Chord.kt
+++ app/src/main/java/com/midichords/model/Chord.kt
@@ -51,6 +51,66 @@ data class Chord(
   }
 
   /**
+   * Get alternative name for this chord, if available.
+   * Some chords can be interpreted in multiple ways.
+   * @param useFlats Whether to use flat notation instead of sharp notation
+   * @return Alternative chord name or null if no alternative exists
+   */
+  fun getAlternativeName(useFlats: Boolean = false): String? {
+    // Check for common alternative interpretations
+    return when {
+      // C/E can also be seen as Em7/C in some contexts
+      type == ChordType.MAJOR && inversion == 1 -> {
+        val thirdAlt = PitchClass.fromPosition((root.position + 4) % 12)
+        "${thirdAlt.getName(useFlats)}m7/${root.getName(useFlats)}"
+      }
+      
+      // C/G can sometimes be interpreted as G11(no3,5,9)
+      type == ChordType.MAJOR && inversion == 2 -> {
+        val fifthAlt = PitchClass.fromPosition((root.position + 7) % 12)
+        "${fifthAlt.getName(useFlats)}sus4(add7)"
+      }
+      
+      // Cmaj7 can be viewed as Em/C
+      type == ChordType.MAJOR_7TH && inversion == 0 -> {
+        val relativeMinor = PitchClass.fromPosition((root.position + 9) % 12)
+        "${relativeMinor.getName(useFlats)}m/${root.getName(useFlats)}"
+      }
+      
+      // C7 can be seen as Edim/Bb in some contexts
+      type == ChordType.DOMINANT_7TH && inversion == 3 -> {
+        val thirdNote = PitchClass.fromPosition((root.position + 4) % 12)
+        "${thirdNote.getName(useFlats)}dim7(no5)"
+      }
+      
+      // Cm7b5 (half-diminished) is the same as Abmaj7/C
+      type == ChordType.HALF_DIMINISHED_7TH -> {
+        val flatSixth = PitchClass.fromPosition((root.position + 8) % 12)
+        "${flatSixth.getName(useFlats)}maj7/${root.getName(useFlats)}"
+      }
+      
+      // G7#11 can be interpreted as D7alt/G
+      type == ChordType.DOMINANT_7_SHARP_11 -> {
+        val fifth = PitchClass.fromPosition((root.position + 7) % 12)
+        "${fifth.getName(useFlats)}7alt/${root.getName(useFlats)}"
+      }
+      
+      // For add9 chords, can be seen as sus2 with added 3rd
+      type == ChordType.ADDED_9TH -> {
+        "$rootName${if (useFlats) "sus2(add3)" else "sus2(add3)"}"
+      }
+      
+      // For quartal chord, can show as stacked 4ths
+      type == ChordType.QUARTAL -> {
+        val fourthUp = PitchClass.fromPosition((root.position + 5) % 12)
+        "${root.getName(useFlats)}-${fourthUp.getName(useFlats)}-${bassNote?.getName(useFlats) ?: "?"} (4ths)"
+      }
+      
+      else -> null
+    }
+  }
+
+  /**
    * Get the full name of this chord.
    * @param useFlats Whether to use flat notation instead of sharp notation
    * @return The full chord name (e.g., "C Major", "D Minor 7th", "G Dominant 7th/B")
@@ -69,9 +129,60 @@ data class Chord(
     
     return if (bassNote != null && bassNote != root) {
       // Include the bass note for slash chords
-      "$rootName $typeName/$bassNote$inversionText"
+      "$rootName $typeName/${bassNote.getName(useFlats)}$inversionText"
     } else {
       "$rootName $typeName$inversionText"
+    }
+  }
+
+  /**
+   * Get jazz-oriented name of this chord with extended notation where applicable
+   * @param useFlats Whether to use flat notation instead of sharp notation
+   * @return The jazz chord name with extended notation
+   */
+  fun getJazzName(useFlats: Boolean = false): String {
+    val rootName = root.getName(useFlats)
+    
+    // Customize names for certain jazz chord types
+    val jazzSymbol = when (type) {
+      ChordType.ALTERED -> "7alt"
+      ChordType.LYDIAN_DOMINANT -> "7#11"
+      ChordType.PHRYGIAN -> "7sus4♭9"
+      ChordType.DOMINANT_13_FLAT_9_SHARP_11 -> "13♭9#11"
+      ChordType.HALF_DIMINISHED_7TH -> "ø"
+      ChordType.DOMINANT_7_SHARP_11 -> "7#11"
+      ChordType.DOMINANT_7_FLAT_13 -> "7♭13"
+      ChordType.QUARTAL -> "4ths"
+      ChordType.SO_WHAT -> "so what"
+      ChordType.DOMINANT_13_FLAT_9 -> "13♭9"
+      ChordType.DOMINANT_13_SHARP_9 -> "13#9"
+      else -> type.symbol
+    }
+    
+    return if (bassNote != null && bassNote != root) {
+      // Include the bass note for slash chords
+      "$rootName$jazzSymbol/${bassNote.getName(useFlats)}"
+    } else {
+      "$rootName$jazzSymbol"
+    }
+  }
+
+  /**
+   * Get the most common voicing of this chord as a list of pitch classes
+   * in the order they would typically be played.
+   * @return List of pitch classes in voicing order
+   */
+  fun getVoicing(): List<PitchClass> {
+    val pitchClasses = getPitchClasses()
+    
+    // For inversions, start with the bass note
+    return if (inversion > 0 && inversion < pitchClasses.size) {
+      // Reorder pitch classes starting with the bass note
+      val invertedClasses = pitchClasses.subList(inversion, pitchClasses.size) +
+                           pitchClasses.subList(0, inversion)
+      invertedClasses
+    } else {
+      pitchClasses
     }
   }
 
@@ -102,6 +213,29 @@ data class Chord(
     }
     
     return Chord(root, type, newInversion, newBassNote)
+  }
+
+  /**
+   * Create a new chord with an explicit bass note, creating a slash chord.
+   * @param bass The bass note to use
+   * @return A new Chord with the specified bass note
+   */
+  fun withBassNote(bass: PitchClass): Chord {
+    if (bass == root) {
+      return this.copy(inversion = 0, bassNote = root)
+    }
+    
+    // Try to find the bass note in the chord to determine inversion
+    val pitchClasses = getPitchClasses()
+    val inversionIndex = pitchClasses.indexOf(bass)
+    
+    return if (inversionIndex > 0) {
+      // This is a standard inversion
+      this.copy(inversion = inversionIndex, bassNote = bass)
+    } else {
+      // This is a non-standard slash chord (bass note not in chord)
+      this.copy(bassNote = bass)
+    }
   }
 
   companion object {
@@ -148,6 +282,25 @@ data class Chord(
      */
     fun minor7(root: PitchClass): Chord {
       return Chord(root, ChordType.MINOR_7TH)
+    }
+
+    /**
+     * Create a slash chord with the specified root chord and bass note.
+     * @param rootChord The root chord
+     * @param bassNote The bass note
+     * @return A slash chord
+     */
+    fun slashChord(rootChord: Chord, bassNote: PitchClass): Chord {
+      return rootChord.withBassNote(bassNote)
+    }
+    
+    /**
+     * Create an altered dominant chord with the specified root.
+     * @param root The root pitch class
+     * @return An altered dominant chord
+     */
+    fun altered(root: PitchClass): Chord {
+      return Chord(root, ChordType.ALTERED)
     }
   }
 } 
--- app/src/test/java/com/midichords/model/ChordTest.kt
+++ app/src/test/java/com/midichords/model/ChordTest.kt
@@ -118,6 +118,18 @@ class ChordTest {
     val am7 = Chord.minor7(PitchClass.A)
     assertEquals(ChordType.MINOR_7TH, am7.type)
     assertEquals(PitchClass.A, am7.root)
+    
+    // Test altered chord factory
+    val g7alt = Chord.altered(PitchClass.G)
+    assertEquals(ChordType.ALTERED, g7alt.type)
+    assertEquals(PitchClass.G, g7alt.root)
+    
+    // Test slash chord factory
+    val c_e = Chord.slashChord(Chord.major(PitchClass.C), PitchClass.E)
+    assertEquals(ChordType.MAJOR, c_e.type)
+    assertEquals(PitchClass.C, c_e.root)
+    assertEquals(PitchClass.E, c_e.bassNote)
+    assertEquals(1, c_e.inversion)
   }
 
   @Test
@@ -147,5 +159,197 @@ class ChordTest {
     val thirdInv = g7.withInversion(3)
     assertEquals(3, thirdInv.inversion)
     assertEquals(PitchClass.F, thirdInv.bassNote)
+  }
+  
+  @Test
+  fun `test withBassNote method`() {
+    // Test creating slash chords with withBassNote method
+    val cMajor = Chord(PitchClass.C, ChordType.MAJOR)
+    
+    // C/E (first inversion) - bass note is part of the chord
+    val cWithE = cMajor.withBassNote(PitchClass.E)
+    assertEquals(1, cWithE.inversion)
+    assertEquals(PitchClass.E, cWithE.bassNote)
+    assertEquals("C/E", cWithE.getName())
+    
+    // C/G (second inversion) - bass note is part of the chord
+    val cWithG = cMajor.withBassNote(PitchClass.G)
+    assertEquals(2, cWithG.inversion)
+    assertEquals(PitchClass.G, cWithG.bassNote)
+    assertEquals("C/G", cWithG.getName())
+    
+    // C/B (non-standard slash chord) - bass note is not part of the chord
+    val cWithB = cMajor.withBassNote(PitchClass.B)
+    assertEquals(0, cWithB.inversion) // Inversion remains 0
+    assertEquals(PitchClass.B, cWithB.bassNote)
+    assertEquals("C/B", cWithB.getName())
+    
+    // Test bass note same as root
+    val cWithC = cMajor.withBassNote(PitchClass.C)
+    assertEquals(0, cWithC.inversion)
+    assertEquals(PitchClass.C, cWithC.bassNote)
+    assertEquals("C", cWithC.getName()) // No slash notation when bass = root
+  }
+  
+  @Test
+  fun `test alternative chord names`() {
+    // Test C/E alternative interpretation as Em7/C
+    val cWithE = Chord(PitchClass.C, ChordType.MAJOR, 1, PitchClass.E)
+    val altName = cWithE.getAlternativeName()
+    assertNotNull(altName)
+    assertEquals("Em7/C", altName)
+    
+    // Test C/G alternative interpretation as Gsus4(add7)
+    val cWithG = Chord(PitchClass.C, ChordType.MAJOR, 2, PitchClass.G)
+    val altName2 = cWithG.getAlternativeName()
+    assertNotNull(altName2)
+    assertEquals("Gsus4(add7)", altName2)
+    
+    // Test Cmaj7 alternative interpretation as Em/C
+    val cMaj7 = Chord(PitchClass.C, ChordType.MAJOR_7TH)
+    val altName3 = cMaj7.getAlternativeName()
+    assertNotNull(altName3)
+    assertEquals("Am/C", altName3)
+    
+    // Test standard C Major - should have no alternative
+    val cMajor = Chord(PitchClass.C, ChordType.MAJOR)
+    val altName4 = cMajor.getAlternativeName()
+    assertNull(altName4)
+    
+    // Test half-diminished chord
+    val bHalfDim = Chord(PitchClass.B, ChordType.HALF_DIMINISHED_7TH)
+    val altName5 = bHalfDim.getAlternativeName()
+    assertNotNull(altName5)
+    assertEquals("G#maj7/B", altName5)
+    assertEquals("Abmaj7/B", bHalfDim.getAlternativeName(true)) // Using flats
+  }
+  
+  @Test
+  fun `test jazz chord names`() {
+    // Test altered dominant
+    val g7alt = Chord(PitchClass.G, ChordType.ALTERED)
+    assertEquals("G7alt", g7alt.getJazzName())
+    
+    // Test Lydian dominant
+    val c7sharp11 = Chord(PitchClass.C, ChordType.LYDIAN_DOMINANT)
+    assertEquals("C7#11", c7sharp11.getJazzName())
+    
+    // Test half-diminished (should use ø symbol)
+    val dHalfDim = Chord(PitchClass.D, ChordType.HALF_DIMINISHED_7TH)
+    assertEquals("Dø", dHalfDim.getJazzName())
+    
+    // Test quartal chord
+    val cQuartal = Chord(PitchClass.C, ChordType.QUARTAL)
+    assertEquals("C4ths", cQuartal.getJazzName())
+    
+    // Test flat-9 dominant 13th
+    val eflat13flat9 = Chord(PitchClass.D_SHARP, ChordType.DOMINANT_13_FLAT_9)
+    assertEquals("D#13♭9", eflat13flat9.getJazzName()) // Using sharps
+    assertEquals("Eb13♭9", eflat13flat9.getJazzName(true)) // Using flats
+    
+    // Test slash chord with jazz notation
+    val g7altWithC = Chord(PitchClass.G, ChordType.ALTERED, 0, PitchClass.C)
+    assertEquals("G7alt/C", g7altWithC.getJazzName())
+  }
+  
+  @Test
+  fun `test chord voicing`() {
+    // Test root position voicing
+    val cMajor = Chord(PitchClass.C, ChordType.MAJOR)
+    val cVoicing = cMajor.getVoicing()
+    assertEquals(3, cVoicing.size)
+    assertEquals(PitchClass.C, cVoicing[0])
+    assertEquals(PitchClass.E, cVoicing[1])
+    assertEquals(PitchClass.G, cVoicing[2])
+    
+    // Test first inversion voicing (E-G-C)
+    val cMajorFirst = cMajor.withInversion(1)
+    val cFirstVoicing = cMajorFirst.getVoicing()
+    assertEquals(3, cFirstVoicing.size)
+    assertEquals(PitchClass.E, cFirstVoicing[0])
+    assertEquals(PitchClass.G, cFirstVoicing[1])
+    assertEquals(PitchClass.C, cFirstVoicing[2])
+    
+    // Test second inversion voicing (G-C-E)
+    val cMajorSecond = cMajor.withInversion(2)
+    val cSecondVoicing = cMajorSecond.getVoicing()
+    assertEquals(3, cSecondVoicing.size)
+    assertEquals(PitchClass.G, cSecondVoicing[0])
+    assertEquals(PitchClass.C, cSecondVoicing[1])
+    assertEquals(PitchClass.E, cSecondVoicing[2])
+    
+    // Test dominant 7th voicing
+    val g7 = Chord(PitchClass.G, ChordType.DOMINANT_7TH)
+    val g7Voicing = g7.getVoicing()
+    assertEquals(4, g7Voicing.size)
+    assertEquals(PitchClass.G, g7Voicing[0])
+    assertEquals(PitchClass.B, g7Voicing[1])
+    assertEquals(PitchClass.D, g7Voicing[2])
+    assertEquals(PitchClass.F, g7Voicing[3])
+    
+    // Test third inversion of dominant 7th (F-G-B-D)
+    val g7Third = g7.withInversion(3)
+    val g7ThirdVoicing = g7Third.getVoicing()
+    assertEquals(4, g7ThirdVoicing.size)
+    assertEquals(PitchClass.F, g7ThirdVoicing[0])
+    assertEquals(PitchClass.G, g7ThirdVoicing[1])
+    assertEquals(PitchClass.B, g7ThirdVoicing[2])
+    assertEquals(PitchClass.D, g7ThirdVoicing[3])
+  }
+  
+  @Test
+  fun `test extended jazz chord formations`() {
+    // Test So What chord (Miles Davis modal voicing)
+    val dSoWhat = Chord(PitchClass.D, ChordType.SO_WHAT)
+    assertEquals("Dso what", dSoWhat.getJazzName())
+    assertEquals(5, dSoWhat.getPitchClasses().size)
+    
+    // Test Quartal harmony
+    val cQuartal = Chord(PitchClass.C, ChordType.QUARTAL)
+    assertEquals("C4ths", cQuartal.getJazzName())
+    assertEquals(4, cQuartal.getPitchClasses().size)
+    
+    // Test complex altered chord
+    val g13b9sharp11 = Chord(PitchClass.G, ChordType.DOMINANT_13_FLAT_9_SHARP_11)
+    assertEquals("G13♭9#11", g13b9sharp11.getJazzName())
+    val pitchClasses = g13b9sharp11.getPitchClasses()
+    assertTrue(pitchClasses.contains(PitchClass.G))  // Root
+    assertTrue(pitchClasses.contains(PitchClass.B))  // Major 3rd
+    assertTrue(pitchClasses.contains(PitchClass.D))  // Perfect 5th
+    assertTrue(pitchClasses.contains(PitchClass.F))  // Dominant 7th
+    assertTrue(pitchClasses.contains(PitchClass.G_SHARP)) // Flat 9th (Ab)
+    assertTrue(pitchClasses.contains(PitchClass.C_SHARP)) // Sharp 11th (C#)
+    assertTrue(pitchClasses.contains(PitchClass.E))  // 13th
+  }
+  
+  @Test
+  fun `test slash chord factory and inversions`() {
+    // Create D/F# chord (first inversion)
+    val dWithFSharp = Chord.slashChord(Chord.major(PitchClass.D), PitchClass.F_SHARP)
+    assertEquals("D/F#", dWithFSharp.getName())
+    assertEquals(PitchClass.D, dWithFSharp.root)
+    assertEquals(PitchClass.F_SHARP, dWithFSharp.bassNote)
+    assertEquals(1, dWithFSharp.inversion)
+    
+    // Create Cm7/Eb chord (first inversion)
+    val cm7WithEb = Chord.slashChord(Chord.minor7(PitchClass.C), PitchClass.D_SHARP) // Eb = D#
+    assertEquals("Cm7/Eb", cm7WithEb.getName(useFlats = true))
+    assertEquals(PitchClass.C, cm7WithEb.root)
+    assertEquals(PitchClass.D_SHARP, cm7WithEb.bassNote)
+    assertEquals(1, cm7WithEb.inversion)
+    
+    // Create G7/D chord (second inversion)
+    val g7WithD = Chord.slashChord(Chord.dominant7(PitchClass.G), PitchClass.D)
+    assertEquals("G7/D", g7WithD.getName())
+    assertEquals(PitchClass.G, g7WithD.root)
+    assertEquals(PitchClass.D, g7WithD.bassNote)
+    assertEquals(2, g7WithD.inversion)
+    
+    // Create C/A chord (non-standard slash chord - A is not in C major)
+    val cWithA = Chord.slashChord(Chord.major(PitchClass.C), PitchClass.A)
+    assertEquals("C/A", cWithA.getName())
+    assertEquals(PitchClass.C, cWithA.root)
+    assertEquals(PitchClass.A, cWithA.bassNote)
+    assertEquals(0, cWithA.inversion) // Inversion is 0 since A is not in C major chord
   }
 } 
